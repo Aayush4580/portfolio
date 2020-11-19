36ff1b3322d72a4bc8fa9929e2cf0c41610079f4
baayush <baayush@deloitte.com>
2020-11-19 16:26:33 +0530

    responsive cards

--- src/component/CustomFlippingCards/CustomFlippingCards.js
+++ src/component/CustomFlippingCards/CustomFlippingCards.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import ReactCardFlip from 'react-card-flip';
-import styles from './CustomFlippingCardsStyle';
 import './CustomFlippingCardsStyle.css';
 
 export const CustomFlippingCards = ({ image, headerText }) => {
@@ -9,13 +8,13 @@ export const CustomFlippingCards = ({ image, headerText }) => {
     <div className="pointer">
       <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
         <div className="frontSide" onMouseEnter={() => setFlipped(!isFlipped)}>
-          <img src={image} alt="" style={styles.frontSideImage} />
+          <img src={image} alt="" className="frontSideImage" />
         </div>
         <div className="backSide" onMouseLeave={() => setFlipped(!isFlipped)}>
           <div>
-            <img src={image} alt="" style={styles.backSideImage} />
+            <img src={image} alt="" className="backSideImage" />
 
-            <div style={styles.backsideText}>{headerText}</div>
+            <div className="backsideText">{headerText}</div>
           </div>
         </div>
       </ReactCardFlip>
