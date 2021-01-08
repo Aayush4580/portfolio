f02f414ee184196a5a8ad69fa307a06ac4535aed
baayush <baayush@deloitte.com>
2021-01-08 16:50:21 +0530

    push

--- src/component/CustomNumberSection/CustomNumberSection.js
+++ src/component/CustomNumberSection/CustomNumberSection.js
@@ -4,76 +4,50 @@ import './CustomNumberSection.css';
 const formatValue = (value) => value.toFixed(0);
 export const CustomNumberSection = () => {
   return (
-    <div>
-      <div className="flexDirectionRow">
-        <div className="numberSubContainer">
-          <div
-            style={{
-              fontSize: '40px',
-              fontWeight: '400',
-              color: '#1aa6e2',
-            }}
-          >
-            <AnimatedNumber
-              value={120000}
-              formatValue={formatValue}
-              duration={1800}
-              delay={1000}
-            />
-          </div>
-          <div>lines of code</div>
+    <div className="flexDirectionRow s">
+      <div className="numberSubContainer">
+        <div className="textStyle">
+          <AnimatedNumber
+            value={120000}
+            formatValue={formatValue}
+            duration={1800}
+            delay={1000}
+          />
         </div>
-        <div className="numberSubContainer">
-          <div
-            style={{
-              fontSize: '40px',
-              fontWeight: '400',
-              color: '#1aa6e2',
-            }}
-          >
-            <AnimatedNumber
-              value={23920000}
-              formatValue={formatValue}
-              duration={1800}
-              delay={1000}
-            />
-          </div>
-          <div>pixels rendered</div>
+        <div>lines of code</div>
+      </div>
+      <div className="numberSubContainer">
+        <div className="textStyle">
+          <AnimatedNumber
+            value={23920000}
+            formatValue={formatValue}
+            duration={1800}
+            delay={1000}
+          />
         </div>
-        <div className="numberSubContainer">
-          <div
-            style={{
-              fontSize: '40px',
-              fontWeight: '400',
-              color: '#1aa6e2',
-            }}
-          >
-            <AnimatedNumber
-              value={52}
-              formatValue={formatValue}
-              duration={1800}
-              delay={1000}
-            />
-          </div>
-          <div>youtube videos</div>
+        <div>pixels rendered</div>
+      </div>
+      <div className="numberSubContainer">
+        <div className="textStyle">
+          <AnimatedNumber
+            value={52}
+            formatValue={formatValue}
+            duration={1800}
+            delay={1000}
+          />
         </div>
-        <div className="numberSubContainer">
-          <div
-            style={{
-              fontSize: '40px',
-              fontWeight: '400',
-              color: '#1aa6e2',
-            }}
-          >
-            <AnimatedNumber
-              value={15}
-              formatValue={formatValue}
-              duration={1800}
-              delay={1000}
-            />
-          </div>
-          <div>projects completed</div>
+        <div>youtube videos</div>
+      </div>
+      <div className="numberSubContainer">
+        <div className="textStyle">
+          <AnimatedNumber
+            value={15}
+            formatValue={formatValue}
+            duration={1800}
+            delay={1000}
+          />
         </div>
+        <div>projects completed</div>
       </div>
     </div>
   );
